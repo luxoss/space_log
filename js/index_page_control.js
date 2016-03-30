82a3546f5df0dac9f388d2c648c182cab855de4d
luxoss <talitan89@gmail.com>
2016-03-31 05:42:49 +0900

    Modified a file

--- js/index_page_control.js
+++ js/index_page_control.js
@@ -7,3 +7,25 @@ function connect_main_display() {
   return ;
 }
 */
+
+document.getElementById("header").style.color = "white";
+
+
+
+
+/*
+// Check user infomation function
+function check_user_info()
+{
+  var id = document.getElementById('username').value;
+  var pwd = document.getElementById('password').value;
+  var email = document.getElementById('email').value;
+  var check_email = document.getElementByid('check_email').value;
+
+  if(){ // If received data is ture, reload a current page.
+    location.reload(true); // Page reload
+  }else{ // Error handling
+    alert('해당 유저 정보가 이미 있습니다. 다시 시도해주세요.');
+  }
+}
+*/
