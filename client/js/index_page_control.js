ede1831c44ef00bfbc9d0d6d933a058a88e644d3
JaesunHan <kapoochino93@gmail.com>
2016-04-02 07:26:00 +0000

    Add folder 'client', 'server' and move files

---
+++ client/js/index_page_control.js
@@ -0,0 +1,34 @@
+/*
+var socket = io.connect('127.0.0.1');
+//socket.emit('form', {id: $('')})
+
+function connect_main_display() {
+
+  return ;
+}
+*/
+
+//Test dom obj in javascript
+var header = document.getElementById('header');
+
+if(typeof header !== 'undefined' && header !== null){
+  document.getElementById('header').style.color = "white";
+}
+
+
+
+
+// Check user infomation function
+function check_user_info()
+{
+  var id = document.getElementById('username').value;
+  var pwd = document.getElementById('password').value;
+  var email = document.getElementById('email').value;
+  var check_email = document.getElementByid('check_email').value;
+
+  if(){ // If received data is ture, reload a current page.
+    location.reload(true); // Page reload
+  }else{ // Error handling
+    alert('해당 유저 정보가 이미 있습니다. 다시 시도해주세요.');
+  }
+}
---
+++ server/test_serv.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var qStr = require('querystring');
+
+http.createServer(function(req, res){
+	if(req.url == '/' && req.method == 'POST'){
+		var postBody = '';
+		req.on('data', function(data){
+			postBody += data;
+		
+		});
+		req.on('end', function(){
+			var post = qStr.parse(postBody);
+			console.log(post);
+		});
+	}else{
+		res.writeHead(404, {'content-type':'text/plain'});
+		res.end('404 ERROR');
+	}
+}).listen(8080);
+
+console.log("Starting server at 8080 port");
