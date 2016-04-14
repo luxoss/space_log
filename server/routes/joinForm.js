df41287e7657197aa18f9ea26960203c1f50d650
Jaesun <jaesun@jaesun-VirtualBox.(none)>
2016-04-14 09:00:52 +0900

    Example Join

--- server/routes/joinForm.js
+++ server/routes/joinForm.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('정상적으로 작동하고 있습니다~');
+  //res.send('정상적으로 작동하고 있습니다~');
+	res.render('joinForm',{title: 'joinForm~~~~'});
+});
+
+router.post('/', function(req,res,next){
+	console.log('req.body : ' + req.body);
+	res.json(req.body);
+
 });
 
 module.exports = router;
