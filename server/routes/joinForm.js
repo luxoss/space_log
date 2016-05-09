a852809b813e66e1f736cfb15e7900fa626a2b5e
JaesunHan <kapoochino93@gmail.com>
2016-05-09 03:49:27 +0000

    adsfasdf

--- server/routes/joinForm.js
+++ server/routes/joinForm.js
@@ -3,18 +3,25 @@ var router = express.Router();
 
 var memObj;//Object that receive member information from client.
 
-//var mongodb = require('mongodb');
-//var MClient = mongodb.MongoClient;
-//var url = 'mongodb://localhost:27017/admin';
+var username, password, email;
 
+var mongodb = require('mongodb');
+var MClient = mongodb.MongoClient;
+var url = 'mongodb://localhost:27017/space_log';
 
-var MongoClient = require('mongodb').MongoClient;
-MongoClient.connect("mongodb://localhost/admin", function(err,db){
-    var adminDB = db.admin()
-    adminDB.listDatabases(function(err, databases){
-        console.log("Before Add Database List: ");
-        console.log(databases);
-    });
+// Use connect method to connect to the Server
+MClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    //HURRAY!! We are connected. :)
+    console.log('Connection established to', url);
+
+    // do some work here with the database.
+
+    //Close connection
+    db.close();
+  }
 });
 
 /* GET users listing. */
@@ -26,11 +33,21 @@ router.post('/', function(req,res,next){
 	console.log('req.body : ' + req.body);
 	memObj = req.body;
 
-	console.log(memObj.username);
-	console.log(memObj.password);
-	console.log(memObj.email);
+	username = memObj.username;
+	password = memObj.password;
+	email = memObj.email;
+
+//	console.log(memObj.username);
+//	console.log(memObj.password);
+//	console.log(memObj.email);
+	console.log(username);
+	console.log(password);
+	console.log(email);
 	res.json(memObj);
 	
 });
+
+
+
 
 module.exports = router;
