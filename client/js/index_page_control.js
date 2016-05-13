a2c983c5d7944ec69b49f432623c578cdb4ad4c1
luxoss <talitan89@gmail.com>
2016-05-13 14:40:23 +0900

    Modified files

--- client/js/index_page_control.js
+++ client/js/index_page_control.js
@@ -1,25 +1,27 @@
 /* Check some user information and enter the 'main page'*/
 function connect_main_display()
 {
-  var socket = new io.Socket(); // Create socket
-//  var username = document.getElementById('username');
-//  var password = document.getElementById('password');
+  var socket = io(); // Create socket
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
   var user_info = new Object();
 
   // Create user_info obj and property that 'username and password'
-  user_info.username = username.value;
-  user_info.password = password.value;
+  user_info.username = username;
+  user_info.password = password;
 
-  // Connect socket 'http://52.79.132.7:8888'
-  socket.connect('http://52.79.132.7:8888/login', {
-    timeout: 3000
+  $('form').submit(function(){
+    socket.connect('http://52.79.132.7:8888/login', {
+      timeout: 3000;
+      socket.emit('message', {'username' : user_info['username'], 'password' : user_info['password']});
+      location.href('../client/main.html');
+    });
+    /*
+    socket.on('connect', function(){
+    });
+    */
   });
 
-  // Send to msg 'user_info'
-  socket.on('connect', function(){
-    socket.emit('message', user_info);
-    location.href('../client/main.html');
-  });
 
   // If dissconn this page, u can see the msg 'dissconnected'
   socket.on('disconnect', function(){
