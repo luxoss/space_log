4d27172b267050dd6ccdbb467d2f6645fe628f03
JaesunHan <kapoochino93@gmail.com>
2016-05-19 06:33:49 +0000

    Modified a file 'main_page_control.js'

--- client/js/main_page_control.js
+++ client/js/main_page_control.js
@@ -2,3 +2,13 @@ function open_popup_view(){
   window.open('index.html', 'pop_up',
               'width=840, height=480, toolbar=no, location=no, status=no, menubar=no, scrollbars=yes, resizable=no, left=300, top=300');
 }
+
+$('logout_btn')
+	.on('click' || 'keydown', function(event){
+		if(event){
+			alert(event):
+		}
+		else{
+		}
+	});
+
