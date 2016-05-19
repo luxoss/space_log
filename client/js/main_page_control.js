6e7356c2af311ac47512341146051031e8d837c4
luxoss <talitan89@gmail.com>
2016-05-19 15:50:58 +0900

    Modified and Added files '.main.html.swp, main_page_control.js, main.html'

--- client/js/main_page_control.js
+++ client/js/main_page_control.js
@@ -1,11 +1,11 @@
+/* Javascript file :: main_page_control */
 function open_popup_view(){
   window.open('index.html', 'pop_up',
               'width=840, height=480, toolbar=no, location=no, status=no, menubar=no, scrollbars=yes, resizable=no, left=300, top=300');
 }
 
-function on_key_down(event.keyCode){
+function on_key_down(){
 	var keycode = event.keyCode;
-
 	alert(keycode);
 };
 
