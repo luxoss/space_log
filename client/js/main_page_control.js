da992c87dd61417854357419434dfb3953640ceb
luxoss <talitan89@gamil.com>
2016-05-20 06:41:06 +0000

    Modified and Added a file '.main_page_control.js.swp, main_page_control.js'

--- client/js/main_page_control.js
+++ client/js/main_page_control.js
@@ -1,4 +1,10 @@
 /* Javascript file :: main_page_control */
+
+var socket = io.connect();
+
+$document.ready(){
+}
+
 function open_popup_view(){
   window.open('index.html', 'pop_up',
               'width=840, height=480, toolbar=no, location=no, status=no, menubar=no, scrollbars=yes, resizable=no, left=300, top=300');
