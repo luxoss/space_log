94d734a1baca70c384882e8ddc2f37d218c3b7a4
luxoss <talitan89@gmail.com>
2016-05-25 18:33:38 +0000

    Modified files 'index.html, js/main_page_control.js, main.html'

--- client/js/main_page_control.js
+++ client/js/main_page_control.js
@@ -1,16 +1,13 @@
 /* Javascript file :: main_page_control */
-
+/*
 $document.ready(function(){
 	var socket = io.connect();
 	
 	socket.on('planet_infor', function(){
 	});
 }
+*/
 
-function open_popup_view(){
-  window.open('index.html', 'pop_up',
-              'width=840, height=480, toolbar=no, location=no, status=no, menubar=no, scrollbars=yes, resizable=no, left=300, top=300');
-}
 
 function on_key_down(){
 	var keycode = event.keyCode;
