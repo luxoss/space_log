6976c9beab0fd605d5c0bfdba4c2d311e10b3c22
JaesunHan <kapoochino93@gmail.com>
2016-06-02 01:57:42 +0000

    ....create planet

--- server/public/javascripts/createPlanet.js
+++ server/public/javascripts/createPlanet.js
@@ -1,11 +1,22 @@
-var 
-
+var MongoClient = require('mongodb').MongoClient;
 
 var last_p_num=0;
-
+var save_p_num=0;
 function create_p(){
 //DB에 행성 정보를 저장하는 걸 여기에 넣기
-		
+	MongoClient.connect("mongodb://localhost/space_log", function(err, db){
+		var adminDB = db.admin();
+		adminDB.listDatabases(function(err, databases){});
+
+		var collection = db.collection("PLANET");
+		last_p_num = collection.count();
+		save_p_num = last_p_num + 1;
+		var x = 10*last_p_num + save_p_num;
+		var y = 15*save_p_num - last_p_num;
+		if(save_p_num<10){
+			collection.insert({planet_id : save_p_num, mineral : 1000, gas : 1000, unknown : 1000, location_x : x, location_y : y});
+		}
+	}); 		
 	
 }
 
