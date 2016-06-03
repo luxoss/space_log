acdd85b6ea0f03d36dffd6cd19a36ae3583431c0
JaesunHan <kapoochino93@gmail.com>
2016-06-03 12:25:48 +0000

    ....create planet

--- server/public/javascripts/createPlanet.js
+++ server/public/javascripts/createPlanet.js
@@ -14,14 +14,25 @@ function create_p(){
 		var x = 10*last_p_num + save_p_num;
 		var y = 15*save_p_num - last_p_num;
 		if(save_p_num<10){
+
 			collection.insert({planet_id : save_p_num, mineral : 1000, gas : 1000, unknown : 1000, location_x : x, location_y : y, create_spd : 1});
+
 		} else if(save_p_num>=10 && save_p_num<100){
-			
+
+			collection.insert({planet_id : save_p_num, mineral : 1000, gas : 800, unknown : 1000, location_x : x, location_y : y, create_spd : 2});
+
 		} else if(save_p_num>=100 && save_p_num<1000){
 			
+			collection.insert({planet_id : save_p_num, mineral : 1000, gas : 800, unknown : 800, location_x : x, location_y : y, create_spd : 3});
+						
 		} else if (save_p_num>=1000 && save_p_num>10000){
 			
+			collection.insert({planet_id : save_p_num, mineral : 800, gas : 800, unknown : 800, location_x : x, location_y : y, create_spd : 4});
+			
+			
 		} else{
+			
+			collection.insert({planet_id : save_p_num, mineral : 600, gas : 600, unknown : 600, location_x : x, location_y : y , create_spd : 5}); 
 			
 		}
 		
