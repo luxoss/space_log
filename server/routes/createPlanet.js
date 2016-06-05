4f19e4f6e8f4e05c4c4592cc163ea858fb40cc81
JaesunHan <kapoochino93@gmail.com>
2016-06-05 09:06:25 +0000

    ....createPlanet.js file

--- server/routes/createPlanet.js
+++ server/routes/createPlanet.js
@@ -9,14 +9,6 @@ function create_p(){
 		adminDB.listDatabases(function(err, databases){});
 
 		var collection = db.collection("PLANET");
-/*		collection.count(function(err, cnt){
-			if(err){
-				console.log('no count');
-			} else if(cnt){
-				
-			}
-			
-		});  */
 		
 		for(var i=0; ;i++){
 			collection.findOne({planet_id : i}, function(err, chck_p_n){
@@ -31,46 +23,30 @@ function create_p(){
 						
 						if(i<10){ //best planet
 							collection.insert({planet_id : i, mineral : 1000, gas : 1000, unknown : 1000, location_x : x, location_y : y, create_spd : 1});
+							
 						} else if(i>=10 && i<100){
 							collection.insert({planet_id : i, mineral : 1000, gas : 800, unknown : 1000, location_x : x, location_y : y, create_spd : 2});
+							
 						} else if(i>=100 && i<1000){
 							collection.insert({planet_id : i, mineral : 1000, gas : 800, unknown : 800, location_x : x, location_y : y, create_spd : 3});
+							
 						} else if(i>=1000 && i<10000){
 							collection.insert({planet_id : i, mineral : 800, gas : 800, unknown : 800, location_x : x, location_y : y, create_spd : 4});
+							
 						} else{ //worst planet
 							collection.insert({planet_id : i, mineral : 800, gas : 600, unknown : 800, location_x : x, location_y : y, create_spd : 5});
+							
 						}
+					} else{
+						console.log('The planet number is already exist.');
 					}
 					
 				}	
 			});	
 		}
-/*		
-		if(save_p_num<10){
-
-			collection.insert({planet_id : save_p_num, mineral : 1000, gas : 1000, unknown : 1000, location_x : x, location_y : y, create_spd : 1});
-i
-		} else if(save_p_num>=10 && save_p_num<100){
-
-			collection.insert({planet_id : save_p_num, mineral : 1000, gas : 800, unknown : 1000, location_x : x, location_y : y, create_spd : 2});
-
-		} else if(save_p_num>=100 && save_p_num<1000){
-			
-			collection.insert({planet_id : save_p_num, mineral : 1000, gas : 800, unknown : 800, location_x : x, location_y : y, create_spd : 3});
-						
-		} else if (save_p_num>=1000 && save_p_num>10000){
-			
-			collection.insert({planet_id : save_p_num, mineral : 800, gas : 800, unknown : 800, location_x : x, location_y : y, create_spd : 4});
-			
-			
-		} else{
-			
-			collection.insert({planet_id : save_p_num, mineral : 600, gas : 600, unknown : 600, location_x : x, location_y : y , create_spd : 5}); 
-			
-		}
-*/		
+	
 	}); 		
 	
 }
 
-//setInterval(create_p, 10000);//1초단위로 create_p 함수를 실행
+setInterval(create_p, 10000);//1초단위로 create_p 함수를 실행
