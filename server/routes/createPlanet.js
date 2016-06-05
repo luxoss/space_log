0782ce38a5da64166f7340a7496bca60dfe17e8d
JaesunHan <kapoochino93@gmail.com>
2016-06-05 07:40:34 +0000

    ...........

--- server/routes/createPlanet.js
+++ server/routes/createPlanet.js
@@ -1,7 +1,7 @@
 var MongoClient = require('mongodb').MongoClient;
 
-var last_p_num=0;
-var save_p_num=0;
+var last_p_num;
+var save_p_num;
 function create_p(){
 //DB에 행성 정보를 저장하는 걸 여기에 넣기
 	MongoClient.connect("mongodb://localhost/space_log", function(err, db){
@@ -9,15 +9,26 @@ function create_p(){
 		adminDB.listDatabases(function(err, databases){});
 
 		var collection = db.collection("PLANET");
+
+		/*
 		last_p_num = collection.count();
 		save_p_num = last_p_num + 1;
-		var x = 10*last_p_num + save_p_num;
-		var y = 15*save_p_num - last_p_num;
 
-		console.log('collection.cout() result is...');
-		console.log(last_p_num);
-	
+		*/
 		
+		var cnt_result = collection.count();
+		
+		console.log('collection.cout() result is...');
+		console.log(cnt_result);
+		
+		last_p_num=cnt_result._id;
+		save_p_num=last_p_num +1;
+		
+		console.log('last_p_num : ' + last_p_num);
+		console.log('save_p_num : ' + save_p_num);
+		var x = 10*last_p_num + save_p_num;
+		var y = 15*save_p_num - last_p_num;	
+/*		
 		if(save_p_num<10){
 
 			collection.insert({planet_id : save_p_num, mineral : 1000, gas : 1000, unknown : 1000, location_x : x, location_y : y, create_spd : 1});
@@ -40,9 +51,9 @@ function create_p(){
 			collection.insert({planet_id : save_p_num, mineral : 600, gas : 600, unknown : 600, location_x : x, location_y : y , create_spd : 5}); 
 			
 		}
-		
+*/		
 	}); 		
 	
 }
 
-//setInterval(create_p, 6000);//1초단위로 create_p 함수를 실행
+//setInterval(create_p, 10000);//1초단위로 create_p 함수를 실행
