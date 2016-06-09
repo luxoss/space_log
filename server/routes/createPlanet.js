c782910d92bab65d0d848bf1e9fea5afed32a55b
JaesunHan <kapoochino93@gmail.com>
2016-06-09 15:58:35 +0900

    .....

--- server/routes/createPlanet.js
+++ server/routes/createPlanet.js
@@ -4,8 +4,9 @@ var io =require('socket.io').listen(5000);
 //var save_p_num;
 var new_p_n;
 var level_p=5;
-
 var source_q, x, y, spd;
+var data, get_time;
+
 
 function create_p(){
 //DB에 행성 정보를 저장하는 걸 여기에 넣기
@@ -20,30 +21,37 @@ function create_p(){
 
 			} else{
 				console.log('count : ' + count);
+
+				date = new Date();
+				get_time = date.getTime();	
+				console.log('get Time : ' + get_time);
+				var t = get_time%10;
+				source_q = (100 + t)*(t%level_p);
+				x = (t+level_p*100)*(t%level_p)+t;
+				y = (t+level_p)*(100-t)-level_p;
+				spd = t%level_p;
+				collection.insert({mineral : source_q, gas : source_q, unknown : source_q, location_x : x, location_y : y , create_spd : spd}, function(err, ins_res){});
+
+
 			//	new_p_n = count+1;
-				var i =0;
-				i=count+1;
-				source_q = (100+i)*(i%level_p);
-				x = (i+level_p)*(i%level_p)+i;
-				y = (i+11)%((i%level_p)+6)*i;
-				spd = i%level_p;
-				
+			//	var i =0;
+			//	i=count+1;
+			//	source_q = (100+i)*(i%level_p);
+			//	x = (i+level_p)*(i%level_p)+i;
+			//	y = (i+11)%((i%level_p)+6)*i;
+			//	spd = i%level_p;
+				/*
 				io.on('connection', function(socket){
-					socket.on('planet_req', function(data){
-						collection.find(function(err, planet){
-							if(err){
-							
-							} else {
-							
-							}
-						});
-					});
+					var planet = collection.find();
+					console.log('PLANET');
+					console.log(planet);
+
+					socket.emit('planet_req', planet);
 					
-				});
-				var j=0;
-				while(j<count){
-				
-				}
+				});*/
+				//console.log(planet);
+
+			
 				/*
 				var i=0;
 				while(i<count){
@@ -69,12 +77,11 @@ function create_p(){
 					
 				}*/
 
-				collection.insert({mineral : source_q, gas : source_q, unknown : source_q, location_x : x, location_y : y , create_spd : spd}, function(err, ins_res){});
-
+				
 			}
 		});
 	}); 		
 	
 }
 
-setInterval(create_p, 86400000);//10초단위로 create_p 함수를 실행
+setInterval(create_p, 100000);//10초단위로 create_p 함수를 실행
