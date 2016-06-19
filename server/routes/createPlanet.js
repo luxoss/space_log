c6d4e00c2551ddfc863e9cdf9f53525b7e16063b
luxoss <kapoochino93@gmail.com>
2016-06-19 16:07:29 +0900

    ....

--- server/routes/createPlanet.js
+++ server/routes/createPlanet.js
@@ -1,6 +1,6 @@
 var MongoClient = require('mongodb').MongoClient;
 
-var io =require('socket.io').listen(5002);
+
 
 var new_p_n;
 var level_p=5;
@@ -45,53 +45,15 @@ function create_p(){
 				collection.insert({mineral : source_q, gas : source_q, unknown : source_q, location_x : x, location_y : y , create_spd : spd}, function(err, ins_res){});
 
 
-				io.on('conection', function(socket){
-					socket.on('planet_req',  function(data){
-						collection.find().toArray(function(err, Pdocs){
-							if (err){
-								console.log('find toArray is error');
-							} else{
-								console.log('planet Documents is .....');
-								for(i=0; i<Pdocs.length; i++){
-									console.log(Pdocs[i]._id);
-								
-								}
-							}
-						});
-
-					});
-				
-				});
-
-			
-				/*
-
-				io.on('connection', function(socket){
-					socket.on('planet_req', function(data){//when client request the all of planet data.
-						var planet = collection.find(function(err, planet){
-							if(err){
-							
-							} else{
-								console.log('planet');
-								console.log(planet);
-								//socket.emit('planet_req', planet);
-							}	
-						});
-					});
-
-
-				});
-
-				*/
 			}
 		});
 	}); 		
 	
 }
 
-//setInterval(create_p, 86400000);//10초단위로 create_p 함수를 실행
+setInterval(create_p, 86400000);//10초단위로 create_p 함수를 실행
 
-setInterval(create_p, 3000);//10초단위로 create_p 함수를 실행
+//setInterval(create_p, 3000);//10초단위로 create_p 함수를 실행
 
 
 //onsole.log('createPlanet.js : http://203.237.179.21:5002');
