5812d4870c073f55560f8472d14fafaacac674e3
luxoss <talitan89@gmail.com>
2016-07-19 03:15:28 +0900

    Modified file's name 'index->indexControl, main->mainControl'

---
+++ client/js/indexControl.js
@@ -0,0 +1,175 @@
+/**
+	*File-name: index.js
+	*Writer: luxoss 
+	*Modified date: 07/16/2016  
+*/
+(function(){ // Create Immediately-invoked function expression
+	$(document).ready(function(){ // Jquery ready to document
+		var socket = io.connect('http://203.237.179.21:5001');
+	
+		mainDisplayResize();
+
+		$("#join_btn").on('click', function(){
+				var username = $('#username').val(); 
+				var password = $('#password').val();
+				var user_info0 = {};
+
+				user_info0.username = username;
+				user_info0.password = password;
+			
+				alert("Loading...");
+
+				if((user_info0.username == " " ) && (user_info0.password == " ") && (user_info0.email == " "))
+				{
+					alert("아이디와 비밀번호를 입력해주세요.");
+					window.location.reload();
+				}
+				else
+				{
+					socket.emit('join_msg', {username: user_info0.username, password: user_info0.password }); // Send to server
+					socket.on('join_res', function(data){ // Receive to server
+				
+						if(data['response'] == 'true')
+						{
+							alert('회원가입이 완료 되었습니다.');
+							window.location.reload();
+						}
+						else
+						{
+							alert('해당 아이디가 이미 있습니다.');
+							window.location.reload();
+						}
+					});
+				}
+		});
+
+		$(document).keydown(function(ev){
+			var key_code = ev.keyCode;
+			
+			if(key_code == 13){
+				var username = $('#username').val();
+				var password = $('#password').val();
+				var main_page_url = "./main.html";
+				var user_info = {}; // Create user information obj		
+		
+				user_info.username = username;
+				user_info.password = password;
+			
+				alert('loading...');
+
+				socket.emit('login_msg', {username: user_info.username, password: user_info.password});
+				
+				socket.on('login_res', function(data){
+					
+					var user_id = user_info.username;
+
+					if(data['response'] == "true")
+					{
+						alert(user_id + "님 space_log 세계에 오신 것을 환영합니다.");
+						usernameValue(user_id);
+						$(location).attr('href', main_page_url);
+					}
+					else
+					{
+						alert("해당 아이디가 이미 있거나 비밀번호가 틀립니다. 다시 시도해 주세요.");
+						$(location).reload();
+					}
+				});
+			}
+		});
+
+		$("#login_btn").on('click', function(){ 
+				var username = $('#username').val();
+				var password = $('#password').val();
+				var main_page_url = "./main.html";
+				var user_info = {}; // Create user information obj		
+		
+				user_info.username = username;
+				user_info.password = password;
+			
+				alert('loading...');
+
+				socket.emit('login_msg', {username: user_info.username, password: user_info.password});
+				
+				socket.on('login_res', function(data){
+					
+					var user_id = user_info.username;
+
+					if(data['response'] == "true")
+					{
+						alert(user_id + "님 space_log 세계에 오신 것을 환영합니다.");
+						usernameValue(user_id);
+						$(location).attr('href', main_page_url);
+					}
+					else
+					{
+						alert("해당 아이디가 이미 있거나 비밀번호가 틀립니다. 다시 시도해 주세요.");
+						$(location).reload();
+					}
+				});
+		});
+	
+		/*
+			// 유저의 정보(자원상태, 초기위치, 레벨 정보 등)을 받기 위해 미리 작성 
+			socket.on('myinfo', function(data){
+				data.username;
+				user_info.exp = data.exp;
+				user_info.mineral = data.mineral;
+				user_info.gas = data.gas;
+				user_info.unknown = data.unknown;
+				user_info.pos_x = data.location_x;
+				user_info.pos_y = data.location_y;
+				console.log('exp: ' + user_info.exp + '\n');
+				console.log('mineral: ' + user_info.mineral + '\n');
+				console.log('gas: ' + user_info.gas + '\n');
+				console.log('unknown: ' + user_info.unknown + '\n');
+				console.log('x: ' + user_info.pos_x + '\n');
+				console.log('y: ' + user_info.pos_y + '\n');
+			});
+		*/
+	
+	});
+
+	function mainDisplayResize()
+	{
+		$(window).resize(function(){
+			$('#main_container').css({position:'absolute'}).css({
+				left: ($(window).width() - $('#main_container').outerWidth()) / 2, 
+				top: ($(window).height() - $('#main_container').outerHeight()) / 2
+			});
+		}).resize();
+		
+		return ;
+	}
+
+	function usernameValue(user_value)
+	{
+		if(!localStorage)
+		{
+			alert("This browser isn'y support localStorage.");
+		}
+		else
+		{	
+			localStorage.setItem('username', user_value);
+		}	
+		return ; 
+	}
+/*	
+	// 회원가입 폼 따로 나뉘어야 할 때 그 페이지에서 쓸 함수 미리 작성 
+	function pwdCheck() 	
+	{
+		var pwd = $('#password').val();
+		var check_pwd = $('#check_password').val();
+
+		if(pwd == check_pwd)
+		{
+			alert("회원가입이 완료되었습니다.");
+		}
+		else
+		{
+			alert("비밀번호가 일치하지 않습니다. 다시 확인해주세요.");
+		}
+		return ;
+	}
+*/
+})();			
---
+++ client/js/mainControl.js
@@ -0,0 +1,398 @@
+/**
+	*File-name: main.js
+	*Writer: luxoss
+	*Date: 07/06/2016 
+*/
+
+(function(){ // Create imediately-invoked function expression
+	//Create socket in global valuable. becuz socket access all document type
+	var server_url =  "http://203.237.179.21"
+	var socket = io.connect(server_url + ":5001");
+	var undiscovered_planet_socket = io.connect(server_url + ":5002");
+	var angle = 0;
+
+	$(document).ready(function(){ // Ready to the document 
+		var url = server_url + ":8000";
+		var user_id = localStorage.getItem('username');
+	
+		//isNaN(_PARAMETER) ? true : false 
+		drawAllAssets();
+		userStateInit(); // Call user state initialize function
+
+		$('#battle_ship_img').append("<div id='" + user_id + "'style='postion:fixed; color: white;'>" + user_id + "</div>");
+
+		$('#logout_btn').on('click', function(){
+			// Below to disconnect user code line 
+			var user_id = localStorage.getItem('username');
+			
+			if(user_id === null)
+			{
+				alert('비 정상적인 로그아웃이므로 게임을 강제 종료합니다.');
+				socket.disconnect();
+				$(location).attr('href', url);	
+			}
+			else
+			{
+				socket.emit('logout_msg', {username: user_id}); 
+				socket.on('logout_res', function(data){
+					if(data.response == 'true')
+					{
+						alert(user_id + ' is logout.');
+						localStorage.removeItem('username');
+						//socket.emit('lpos_req', {'x': pos_x, 'y': pos_y}); 
+						socket.disconnect();
+						$(location).attr('href', url);
+					}
+					else if(data.response == 'false')
+					{
+						alert('Logout error.');
+					};
+				});
+			}
+		});	
+
+		$('#planet_btn').on('click', function(){
+			planetViewLayer();
+		});
+
+		$('#battle_ship_btn').on('click', function(){
+			alert('Click battle ship button.');
+			battleShipViewLayer();
+		});
+
+		$('#rank_btn').on('click', function(){
+			alert('Click rank button.');
+			rankViewLayer();
+		});
+	});
+
+	$(document).keydown(function(e){ // Create key press down event 
+		/*
+			38 : up
+			40 : down 
+			37 : left
+			39 : right
+			83 : S key is 'Shot'
+			66 : B key is 'battle ship button'
+			82 : R key is 'Rank button'
+			80 : P key is 'Planet information button'
+		*/
+		var key_down_event = e.keyCode;	
+
+		switch(key_down_event)
+		{
+			case 38: // up key press down
+				$('#battle_ship_img').animate({top: "-=50"}, {queue: false});
+				// rotate_translation(top, left) top, left is 'coordinate x, y';
+				// [x', y'] =  [x, y][{sin(angle), cos(angle)}, {cos(angle), sin(angle)}] is right rotate translation
+				break;
+			case 40: // down key press down
+				$('#battle_ship_img').animate({top: "+=50"}, {queue: false});
+				break;
+			case 37: // left key press down
+		        	$('#battle_ship_img').css('transform',  'rotate(' + angle + 'deg)');
+//				player_pos = clockwiseRotateTransform(x, y, angle);
+//				player_pos[2] -= 30;
+				angle -= 30;
+				break;
+			case 39: // right key press down
+				$('#battle_ship_img').css('transform',  'rotate(' + angle + 'deg)');
+//				counterClockwiseRotateTransform(x, y, angle);
+//				player_pos[2] += 30;
+				angle += 30;
+				break;
+			case 83:
+				alert('shot button');
+				break;
+			case 66:
+				battleShipViewLayer(); // call function battle ship layer
+				break;
+			case 82:
+				rankViewLayer(); 	  // call function rank layer
+				break;
+			case 80:
+				planetViewLayer();	  // call function planet layer
+				break;
+			case 81:
+				logout();
+				break;
+			default:
+				break;
+		};
+		return ; 
+	});
+
+	$(document).keyup(function(ev){ // Key press up event 
+		var battle_ship_pos = {}; // Create battle ship position set object and send to server
+		var key_up_event = ev.keyCode;	
+	
+		switch(key_up_event)
+		{
+			case 38:
+				$('#battle_ship_img').stop();
+				break;
+			case 40: 
+				$('#battle_ship_img').stop();
+				break;
+			case 37:
+				$('#battle_ship_img').stop();
+				break;
+			case 39:
+				$('#battle_ship_img').stop();			
+				break;
+			case 83:
+				alert('shot button');
+				break;
+			default:
+				break;
+		};
+		
+	});	
+	
+	// Create draw background image in canvas 
+	function drawAllAssets()
+	{
+		undiscovered_planet_socket.emit('planet_req', {'ready' : 'ready to connect planet db'});
+		undiscovered_planet_socket.on('planet_res', function(data){
+			console.log(data);
+/*
+			undiscovered_planet_info.id = data._id;
+			undiscovered_planet_info.x = data.location_x;
+			undiscovered_planet_info.y = data.location_y;
+			undiscovered_planet_info.gas = data.gas;
+			undiscovered_planet_info.mineral = data.mineral;
+			undiscovered_planet_info.unknown = data.unknown;
+			undiscovered_planet_info.grade = data.create_spd;
+*/	
+			// Create group of the planet grage image obj
+			var planet1_img = new Image();
+			var planet2_img = new Image();
+			var planet3_img = new Image();
+			var planet4_img = new Image();
+
+			planet1_img.src = server_url + ":8000/res/img/planet/planet_9.png";
+			planet2_img.src = server_url + ":8000/res/img/planet/planet_11.png";
+			planet3_img.src = server_url + ":8000/res/img/planet/planet_12.png";
+			planet4_img.src = server_url + ":8000/res/img/planet/planet_13.png";
+
+			var canvas = document.getElementById('background');
+			var planet_img = new Image();
+			var pos_x = parseInt(data.location_x);
+			var pos_y = parseInt(data.location_y);
+	
+			if(canvas.getContext)
+			{
+				context = canvas.getContext('2d');
+				context.fillStyle = 'Black';
+				context.rect(0, 0, 2048, 1024);
+				context.fill();
+				
+				drawStars();
+			}
+
+			planet_img.onload = function()
+			{
+				context.drawImage(planet_img, pos_x, pos_y, 100, 100);
+				console.log(pos_x, pos_y);
+			}
+			planet_img.src = server_url + ":8000/res/img/planet/planet_11.png";
+		});
+		return ;
+	}
+
+	function drawStars()
+	{
+		for(var i=0; i<=100; i++)
+		{
+			var x = Math.floor(Math.random() * 2047);
+			var y = Math.floor(Math.random() * 1023);
+			
+			context.fillStyle = "rgb(255, 255, 0)";
+			
+			context.beginPath();
+			context.arc(x, y, 3, 0, Math.PI * 2, true);
+			context.closePath();
+			context.fill();
+		}
+		return ; 
+	}
+
+	// Create user state function in main display
+	function userStateInit()
+	{
+		$(window).resize(function(){		
+			$('#user_obj').css({
+				left: ($(window).width() - $('#user_obj').outerWidth()) / 2,
+				top: ($(window).height() - $('#user_obj').outerHeight()) / 2
+			});
+
+			$('#battle_ship_img').css({
+				left: ($(window).width() - $('#battle_ship_img').outerWidth()) / 2,
+				top: ($(window).height() - $('#battle_ship_img').outerHeight()) / 2
+			});
+		}).resize();
+		return ;
+	}
+
+	// Create planet menu controller function
+	function planetViewLayer()
+	{  
+		var state = $('#planet_layer').css('display');
+		var undiscovered_planet_info = {};
+	
+		undiscovered_planet_info.x = null;
+		undiscovered_planet_info.y = null;
+		undiscovered_planet_info.gas = null;
+		undiscovered_planet_info.mineral = null;
+		undiscovered_planet_info.undiscovered = null;
+		
+		//response undiscovered plnaet database 
+		undiscovered_planet_socket.emit('planet_req', {'ready' : 'ready to connect planet db'});
+		undiscovered_planet_socket.on('planet_res', function(data){
+			console.log(data);	
+			undiscovered_planet_info.id = data._id;
+			undiscovered_planet_info.x = data.location_x;
+			undiscovered_planet_info.y = data.location_y;
+			undiscovered_planet_info.gas = data.gas;
+			undiscovered_planet_info.mineral = data.mineral;
+			undiscovered_planet_info.unknown = data.unknown;
+			undiscovered_planet_info.grade = data.create_spd;
+		
+			$('#main_layer').append("<div id='" + data._id + "' style='position: absolute; color: white; top: " + data.location_x + "; left:" + data.location_y + "; width: 100px; height: 100px;'>" + undiscovered_planet_img + "</div>");	
+
+			$('#planet_layer').append("<div id='" + data._id + "' style='position: absolute; color: white; top: " + data.location_x + "; left:" + data.location_y + "; width: 100px; height: 100px;'>" + undiscovered_planet_img + "</div>");
+	
+		});
+
+		$(window).resize(function(){
+			$('#planet_layer').css({
+				left: ($(window).width() - $('#planet_layer').outerWidth()) / 2,
+				top: ($(window).height() - $('#planet_layer').outerHeight()) / 2
+			});
+		}).resize();
+		
+		if(state == 'none')
+		{
+			$('#planet_layer').show();
+		}
+		else
+		{
+			$('#planet_layer').hide();
+		}
+
+		return ; 		
+	}
+
+	// Create battle ship menu controller function
+	function battleShipViewLayer(){	
+		var state = $('#battle_ship_layer').css('display');
+	
+		$(window).resize(function(){
+			$('#battle_ship_layer').css({
+				left: ($(window).width() - $('#battle_ship_layer').outerWidth()) / 2,
+				top: ($(window).height() - $('#battle_ship_layer').outerHeight()) / 2
+			});
+		}).resize();
+
+		if(state == 'none')
+		{
+			$('#battle_ship_layer').show();
+		}
+		else
+		{
+			$('#battle_ship_layer').hide();
+		}
+	
+		return ;
+	}
+
+	// Create rank menu controller function
+	function rankViewLayer()
+	{	
+		var state = $('#rank_layer').css('display');
+		
+		$(window).resize(function(){
+			$('#rank_layer').css({
+				left: ($(window).width() - $('#rank_layer').outerWidth()) / 2,
+				top: ($(window).height() - $('#rank_layer').outerHeight()) / 2
+			});
+		}).resize();
+
+		if(state == 'none')
+		{
+			$('#rank_layer').show();
+		}
+		else
+		{
+			$('#rank_layer').hide();
+		}
+		return ; 
+	}
+
+			
+	// Create clockwise rotate transformation matrix function
+	function clockwiseRotateTransform(x, y, angle)
+	{
+		var pos_x, pos_y;
+
+		var ratio = {
+			x : Math.cos(angle),
+			y : Math.sin(angle)
+		};
+
+		pos_x = ((x * ratio.x) + (y * (-ratio.y)));
+		pos_y = ((x * ratio.y) + (y * (ratio.x)));
+
+		angle += 30;
+			
+		return ;
+	}
+
+	// Create counter clockwise rotate tranformation matrix function
+	function counterClockwiseRotateTransform(x, y, angle)
+	{
+		var pos_x, pos_y;
+
+		var ratio = {
+			x : Math.cos(angle),
+			y : Math.sin(angle)
+		};
+
+		pos_x = ((x * ratio.x) + (y * (ratio.y)));
+		pos_y = ((x * -ratio.y) + (y * (ratio.x)));
+
+		angle += 30;
+
+		return ;	
+	}
+
+	function logout()
+	{
+		var logout_user_id = localStorage.getItem('username');
+		var logout_msg = confirm('로그아웃 하시겠습니까?');
+		var main_url = server_url + ":8000";
+
+		if(logout_msg == true)
+		{
+			socket.emit('logout_msg', {username: logout_user_id}); 
+			socket.on('logout_res', function(data){
+				if(data.response == 'true')
+				{
+					alert(logout_user_id + ' is logout.');
+					localStorage.removeItem('username');
+					//socket.emit('lpos_req', {'x': pos_x, 'y': pos_y}); 
+					socket.disconnect();
+					$(location).attr('href', main_url);
+				}
+				else if(data.response == 'false')
+				{
+					alert('Logout error.');
+				};
+			});
+		}
+		return ;
+	}
+})();
+
+
+
