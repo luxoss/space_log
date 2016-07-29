ed199f53695126f3ed27cdf833570812c07643ef
luxoss <talitan89@gmail.com>
2016-07-30 01:01:28 +0900

    Modified files 'battleShipUi.js, keyController.js, mainControl.js, planetUi.js, rankUi.js'

--- client/js/rankUi.js
+++ client/js/rankUi.js
@@ -3,4 +3,27 @@
 	**Writer: luxoss
 	**Modified date: 07/29/2016
 */
+var rankViewLayer = function(){
+	
+	// Create rank menu controller function
+	var state = $('#rank_layer').css('display');
 
+	if(state == 'none')
+	{
+		$('#rank_layer').show();
+	}
+	else
+	{
+		$('#rank_layer').hide();
+	}
+	
+	$(window).resize(function(){
+		$('#rank_layer').css({
+			left: ($(window).width() - $('#rank_layer').outerWidth()) / 2,
+			top: ($(window).height() - $('#rank_layer').outerHeight()) / 2
+		});
+	}).resize();
+
+	return ; 
+};
+	
