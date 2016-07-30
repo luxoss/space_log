7997c26a770ea3da746c10436bf995517f64e052
luxoss <talitan89@gmail.com>
2016-07-31 00:44:32 +0900

    Added files  'login.js, main.js'

---
+++ client/js/login.js
@@ -0,0 +1,177 @@
+/**
+	*File-name: index.js
+	*Writer: luxoss 
+	*Modified date: 07/16/2016  
+*/
+(function(){ // Create Immediately-invoked function expression
+	$(document).ready(function(){ // Jquery ready to document
+		var socket = io.connect('http://203.237.179.21:5001');
+	
+		mainDisplayResize();
+
+		$("#join_btn").on('click', function(){
+				var username = $('#username').val(); 
+				var password = $('#password').val();
+				var userInfo0 = {};
+
+				userInfo0.username = username;
+				userInfo0.password = password;
+			
+				alert("Loading...");
+
+				if((userInfo0.username == " " ) && (userInfo0.password == " "))
+				{
+					alert("아이디와 비밀번호를 입력해주세요.");
+					window.location.reload();
+				}
+				else
+				{
+					socket.emit('join_msg', {username: userInfo0.username, password: userInfo0.password }); // Send to server
+					socket.on('join_res', function(data){ // Receive to server
+				
+						if(data['response'] == 'true')
+						{
+							alert('회원가입이 완료 되었습니다.');
+							window.location.reload();
+						}
+						else
+						{
+							alert('해당 아이디가 이미 있습니다.');
+							window.location.reload();
+						}
+					});
+				}
+		});
+
+		$(document).keydown(function(ev){
+
+			var keyCode = ev.keyCode;
+			
+			if(keyCode == 13)
+			{
+				var username = $('#username').val();
+				var password = $('#password').val();
+				var mainPageUrl = "./main.html";
+				var userInfo = {}; // Create user information obj		
+		
+				userInfo.username = username;
+				userInfo.password = password;
+			
+				alert('loading...');
+
+				socket.emit('login_msg', {username: userInfo.username, password: userInfo.password});
+				
+				socket.on('login_res', function(data){
+					
+					var userId = userInfo.username;
+
+					if(data['response'] == "true")
+					{
+						alert(userId + "님 space_log 세계에 오신 것을 환영합니다.");
+						usernameValue(userId);
+						$(location).attr('href', mainPageUrl);
+					}
+					else
+					{
+						alert("해당 아이디가 이미 있거나 비밀번호가 틀립니다. 다시 시도해 주세요.");
+						$(location).reload();
+					}
+				});
+			}
+		});
+
+		$("#login_btn").on('click', function(){ 
+				var username = $('#username').val();
+				var password = $('#password').val();
+				var mainPageUrl = "./main.html";
+				var userInfo = {}; // Create user information obj		
+		
+				userInfo.username = username;
+				userInfo.password = password;
+			
+				alert('loading...');
+
+				socket.emit('login_msg', {username: userInfo.username, password: userInfo.password});
+				
+				socket.on('login_res', function(data){
+					
+					var userId = userInfo.username;
+
+					if(data['response'] == "true")
+					{
+						alert(userId + "님 space_log 세계에 오신 것을 환영합니다.");
+						usernameValue(userId);
+						$(location).attr('href', mainPageUrl);
+					}
+					else
+					{
+						alert("해당 아이디가 이미 있거나 비밀번호가 틀립니다. 다시 시도해 주세요.");
+						$(location).reload();
+					}
+				});
+		});
+	
+		/*
+			// TODO: Received 'USER'S INFORMATION(Resource, Initialize postion, Level, and so on)' to SERVER
+			socket.on('myinfo', function(data){
+				data.username;
+				user_info.exp = data.exp;
+				user_info.mineral = data.mineral;
+				user_info.gas = data.gas;
+				user_info.unknown = data.unknown;
+				user_info.pos_x = data.location_x;
+				user_info.pos_y = data.location_y;
+				console.log('exp: ' + user_info.exp + '\n');
+				console.log('mineral: ' + user_info.mineral + '\n');
+				console.log('gas: ' + user_info.gas + '\n');
+				console.log('unknown: ' + user_info.unknown + '\n');
+				console.log('x: ' + user_info.pos_x + '\n');
+				console.log('y: ' + user_info.pos_y + '\n');
+			});
+		*/
+	
+	});
+
+	function mainDisplayResize()
+	{
+		$(window).resize(function(){
+			$('#main_container').css({position:'absolute'}).css({
+				left: ($(window).width() - $('#main_container').outerWidth()) / 2, 
+				top: ($(window).height() - $('#main_container').outerHeight()) / 2
+			});
+		}).resize();
+		
+		return ;
+	}
+
+	function usernameValue(userValue)
+	{
+		if(!localStorage)
+		{
+			alert("This browser isn'y support localStorage.");
+		}
+		else
+		{	
+			localStorage.setItem('username', userValue);
+		}	
+		return ; 
+	}
+/*	
+	//TODO: Check 'join form '
+	function pwdCheck() 	
+	{
+		var pwd = $('#password').val();
+		var check_pwd = $('#check_password').val();
+
+		if(pwd == check_pwd)
+		{
+			alert("회원가입이 완료되었습니다.");
+		}
+		else
+		{
+			alert("비밀번호가 일치하지 않습니다. 다시 확인해주세요.");
+		}
+		return ;
+	}
+*/
+})();			
---
+++ client/js/main.js
@@ -0,0 +1,163 @@
+/**
+	*File name: main.js
+	*Writer: luxoss
+	*Modified date: 07/30/2016 
+	
+	**Variable naming: camelCase 
+	**Function naming: camelCase
+	**jquery selector naming: under_bar
+*/
+
+(function(){ // Create imediately-invoked function expression
+	//Create socket global scope. becuz socket access all document type
+	var serverUrl =  "http://203.237.179.21"
+	var socket = io.connect(serverUrl + ":5001");
+	var undiscoveredPlanetSocket = io.connect(serverUrl + ":5002");
+	//var battleShipObj = $("#battle_ship").offset(); 
+	//var positionX = battleShipObj.left;
+	//var positionY = battleShipObj.top;
+	var userId = localStorage.getItem("username");
+
+	$(document).ready(function(){ // Ready to the document 
+
+		var indexPageUrl = serverUrl + ":8000";
+	
+		//isNaN(_PARAMETER) ? true : false 
+		drawAllAssets();
+		userStateInit(); // Call user state initialize function
+
+		$('#battle_ship').append("<div id='" + userId + "'style='postion:fixed; color: white;'>" + userId + "</div>");
+
+		$('#logout_btn').on('click', function(){
+			// Below to disconnect user code line 
+			
+			if(userId === null)
+			{
+				alert('비 정상적인 로그아웃이므로 게임을 강제 종료합니다.');
+
+				socket.disconnect();
+
+				$(location).attr('href', indexPageUrl);	
+			}
+			else
+			{
+				logout(userId);
+			}
+		});	
+
+		$('#planet_btn').on('click', function(){
+			planetViewLayer();
+		});
+
+		$('#battle_ship_btn').on('click', function(){
+			alert('Click battle ship button.');
+			battleShipViewLayer();
+		});
+
+		$('#rank_btn').on('click', function(){
+			alert('Click rank button.');
+			rankViewLayer();
+		});
+	});
+	
+	// Create draw background image in canvas 
+	function drawAllAssets()
+	{
+		undiscoveredPlanetSocket.emit('planet_req', {'ready' : 'ready to connect planet db'});
+
+		undiscoveredPlanetSocket.on('planet_res', function(data){
+
+			// Create image obj
+			var canvas = document.getElementById("background");
+			var posX = parseInt(data.location_x);
+			var posY = parseInt(data.location_y);
+			var planetImg = new Image();
+	
+			planetImg.src = serverUrl + ":8000/res/img/planet/planet_13.png";
+
+			if(canvas.getContext)
+			{
+				context = canvas.getContext('2d');
+				context.fillStyle = 'Black';
+				context.rect(0, 0, 2048, 1024);
+				context.fill();
+				
+				for(var i = 0; i <= 100; i++)
+				{
+					var starX = Math.floor(Math.random() * 2047);
+					var starY = Math.floor(Math.random() * 1023);
+			
+					context.fillStyle = "rgb(255, 255, 0)";
+					
+					context.beginPath();
+					context.arc(starX, starY, 3, 0, Math.PI * 2, true);
+					context.closePath();
+					context.fill();
+				}
+				
+			}
+			
+			planetImg.onload = function()
+			{
+				context.drawImage(planetImg, posX, posY, 100, 100);
+			}
+
+		});
+	
+		return ;
+	}
+
+	// Create user state function in main display
+	function userStateInit()
+	{
+		var canvas = document.getElementById('background');
+		var battleShipImg = new Image();
+	
+		$(window).resize(function(){		
+
+			$('#battle_ship').css({
+				left: ($("#background").width() - $('#battle_ship').outerWidth()) / 2,
+				top: ($("#background").height() - $('#battle_ship').outerHeight()) / 2
+			});
+
+		}).resize();
+
+		return ;
+	}
+
+	function logout(logoutUserId)
+	{
+		var logoutMsg = confirm('로그아웃 하시겠습니까?');
+		var indexPageUrl = serverUrl + ":8000";
+
+		if(logoutMsg == true)
+		{
+			socket.emit('logout_msg', {username: logoutUserId}); 
+
+			socket.on('logout_res', function(data){
+
+				if(data.response == 'true')
+				{
+					alert(logoutUserId + '님께서 로그아웃 되셨습니다.');
+
+					localStorage.removeItem('username');
+
+					//socket.emit('lpos_req', {'x': posX, 'y': posY}); 
+					socket.disconnect();
+
+					$(location).attr('href', indexPageUrl);
+				}
+				else if(data.response == 'false')
+				{
+					alert('Logout error.');
+				};
+
+			});
+		}
+
+		return ;
+	}
+})();
+
+
+
