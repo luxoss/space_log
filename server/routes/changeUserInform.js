c6c6e5e98ee3969954e623e49166a13039772c96
JaesunHan <kapoochino93@gmail.com>
2016-08-07 11:16:44 +0900

    Modified File ChangeUserInfrom.js..... working socket programming

--- server/routes/changeUserInform.js
+++ server/routes/changeUserInform.js
@@ -5,6 +5,8 @@ var router = express.Router();
 var username;
 var g_mineral, g_gas, g_unknown;
 var g_exp;
+var s_mineral, s_gas, s_unknown;
+var s_exp;
 
 var MongoClient = require('mongodb').MongoClient;
 
@@ -23,12 +25,34 @@ io.on('connection', function(socket){
 		g_gas = data.gas;
 		g_unknown = data.unknown;
 
+		MongoClinet.connect("mongodb://localhost/space_log", function(err, db){
+			var collection = db.collection("MEM_INFO");
+			var f_obj = {"username" : username};
+
+			collection.findOne(f_obj, function(err, user){
+				if(err){
+					console.log("ChangeUserInform.js file's error. There is no data that matching username");				
+					console.log("=======ERROR MESSAGE======");
+					console.log(err);
+				} else if(user){
+				
+				}
+
+			});
+			
+		});
+
+
 		
 	});
 
 	//user get exp
 	socket.on('get_exp', function(data){
-		
+		username = data.username;
+		g_exp = data.exp;
+
+		// s_epx = mongoClinet.collection("PLANET").exp + g_exp
+		s_exp = 
 	
 	});
 
---
+++ server/routes/changeUserPlanet.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var router = express.Route();
+
+var username;
+var p_id;
+
+var MongoClient = require('mongodb').MongoClient;
+
+var io = require('socket.io').listen(5004);
+
+router.get('/', function(req, res, next){
+	res.send('respond with a resource');
+});
+
+io.on('connection', function(socket){
+	//user get Planet -> add to DB
+	socket.on('add_p', function(data){
+	
+	});
+
+	//user remove planet -> delete to DB
+	socket.on('rmv_p', function(data){
+	
+	});
+});
