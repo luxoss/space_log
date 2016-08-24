46018002e8530ebfcc8f34816822ca9168b605f2
luxoss <talitan89@gmail.com>
2016-08-25 02:02:47 +0900

    Added and Modified are files

---
+++ client/js/sound.js
@@ -0,0 +1,49 @@
+/**
+	**File-name: sound.js
+	**Writer: luxoss
+	**File-explanation: THis file control the all sounds
+*/
+
+// Create sound object
+var sound = function() {
+	// Preload sound
+	this.preload = function(url) {
+		// TODO: Later...
+	};
+
+	// If sound is preloaded, return true
+	this.isPreloaded = function() {
+		// TODO: Later...
+	};
+
+	// Play this sound 
+	// If turn on the loop, repeat infinit play sound until sound is paused 
+	this.play = function(loop) {
+		// TODO: Later...
+	}; 
+
+	// Pause this sound
+	this.stop = function() {
+		// TODO: Later...
+	};
+};
+
+var initialize = function() {
+	// TODO: Later...
+	backgroundMusic = new sound();
+	backgroundMusic.preload(/*Insert background music url*/);
+	waitForSound();
+};
+
+
+var waitForSound = function() {
+	if(backgroundMusic.isPreloaded()) {
+		// TODO: Later...
+		backgroundMusic.play(true);
+	} 
+	else {
+		setTimeout(arguments.callee, 100);
+	}
+};
+
+
