64b074d6ee2a0de3c8f4b2bf52412b15238eedac
luxoss <talitan89@gmail.com>
2016-09-10 05:05:03 +0900

    Modified a file 'minimap_ui.js'

--- client/js/minimap_ui.js
+++ client/js/minimap_ui.js
@@ -4,12 +4,7 @@
 	File explanation: Control minimap canvas
 */
 
-var canvas = $('.minimap_canvas'); 
-var width = 0;  // width 값은 전체 가로 크기에서 미니맵 가로 크기로 나누어 parseInt 해볼 것 
-var height = 0; // 위와 같은 의미
-
 // 행성 위치에 관한 목록을 소캣으로 받는다.
-
 // 유저 위치에 관한 목록을 소캣으로 받는다. 
 
 // 각각의 유저 요소와 행성요소를 그려주기위한 함수  
