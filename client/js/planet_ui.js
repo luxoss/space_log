641cff188efd8c050cb8667eecd21aac7324374c
luxoss <talitan89@gmail.com>
2016-09-28 00:37:57 +0900

    Modified a file 'planet_ui.js'

--- client/js/planet_ui.js
+++ client/js/planet_ui.js
@@ -6,64 +6,56 @@
 
 function planetViewLayer(planetSocket)
 {
-	var state = $('.planet_ui').css('display');
-	console.log(typeof planetSocket);
-	console.log(planetSocket);
+   var state = $('.planet_ui').css('display');
+      console.log(planetSocket);
 
-	if(state == 'none') {
-		$('.planet_ui').show();
+   if(state == 'none') 
+   {
+      $('.planet_ui').show();
 
-		planetSocket.emit('planet_req', {'receive' : 'that'});
+      planetSocket.emit('planet_req', {'ready' : 'client=>ready to receive planet information db'});
 
-		planetSocket.on('planet_res', function(data){
-
-			console.log('socket is alive.');
-			var planet = {
-				name : data._id,
-				gas : data.gas,
-				mineral : data.mineral,
-				unknown : data.unknown,
-				//discover : "false",
-				grade : data.create_spd
-			};
+      planetSocket.on('planet_res', function(data){
+         console.log('socket is alive.');
+	 var planet = {
+	    name : data._id,
+	    gas : data.gas,
+	    mineral : data.mineral, 
+            unknown : data.unknown,
+	    //discover : "false",
+	    grade : data.create_spd
+	 };
 			
-			console.log(
-				planet.name,
-				planet.gas,
-				planet.mineral,
-				planet.unknown,
-				planet.grade
-			);
+	 console.log("planet: ", "gas: ", "mineral: ", "unknown: ", "grade: ",
+                      planet.name, planet.gas, planet.mineral, planet.unknown, planet.grade);
 
-			$("#planet_name").append(
-				"<div id ='" + planet.name + "' style='position: absolute;'></div>"
-			);
+	 $("#planet_name").append(
+            "<div id ='" + planet.name + "' style='position: absolute;'></div>"
+	 );
 
-			$("#planet_resource").append(
-				"<div id='" + planet.gas + "'style='position:absolute;'></div>"	
-				+ "<div id='" + planet.mineral + "'style='position:absolute;'></div>" 
-				+ "<div id='" + planet.unknown + "'style='position:absolute;'></div>"
-			);
+	 $("#planet_resource").append(
+	    "<div id='" + planet.gas + "'style='position:absolute;'></div>"	
+	    + "<div id='" + planet.mineral + "'style='position:absolute;'></div>" 
+	    + "<div id='" + planet.unknown + "'style='position:absolute;'></div>"
+	 );
 
-			//$("#planet_discovered").append(planet.discover);
-			$("#planet_grade").append(
-				"<div id='" + planet.grade + "'style='position:absolute;'></div>"
-			); 
+	 //$("#planet_discovered").append(planet.discover);
+	 $("#planet_grade").append(
+	    "<div id='" + planet.grade + "'style='position:absolute;'></div>"
+	 ); 
+      });
+   }
+   else 
+   {
+      $('.planet_ui').hide();
+   }
 
-		});
-	}
-	else {
-		$('.planet_ui').hide();
-	}
-
-	$(window).resize(function(){
-
-		$('.planet_ui').css({
-			left: ($(window).width() - $('.planet_ui').outerWidth()) / 2,
-			top: ($(window).height() - $('.planet_ui').outerHeight()) / 2
-		});
-
-	}).resize();		
+   $(window).resize(function(){
+      $('.planet_ui').css({
+         left: ($(window).width() - $('.planet_ui').outerWidth()) / 2,
+	 top: ($(window).height() - $('.planet_ui').outerHeight()) / 2
+      });
+   }).resize();		
 }
 
 
