58ef4dc0bd4c9065d8ceb5ee5bdd273e2eaa475b
luxoss <talitan89@gmail.com>
2016-10-09 00:50:37 +0900

    Modified files

--- server/routes/broadcastPos.js
+++ server/routes/broadcastPos.js
@@ -21,14 +21,14 @@ UsersPio.on('connection', function(socket){
 		var member = db.collection("MEMBER");
 
 		socket.on('press_key', function(data){
-			console.log("Client press arrow key");
+			console.log("[SERVER LOG] Arrow key is pressd by", data.username, "client");
 			console.log(data.ready);
 //			var mem_info = db.collection("MEM_INFO");
 //			var member = db.collection("MEMBER");			
-			username=data.username;
-			x=data.location_x;
-			y=data.location_y;
-			key_val=data.key_val;
+			username = data.username;
+			x = data.location_x;
+			y = data.location_y;
+			key_val = data.key_val;
 
          if(key_val == LEFT) {
             //my_obj.x -= speed;
@@ -45,11 +45,12 @@ UsersPio.on('connection', function(socket){
          }else {
          }
 
-			mem_info.update({"username":username}, {$set : {"location_x":x,"location_y":y, "key_val":key_val}});
+			mem_info.update({"username":username}, {$set : { "location_x" : x, "location_y" : y, "key_val" : key_val }});
 			
 			mem_info.findOne({"username":username}, function(err, res_info){
+
 				if(err){
-					 console.log('branch if : err');
+					 console.log("[SERVER LOG] Branch if : err");
 					 console.log(err);
 				} else if (res_info){
 					mv_obj = {
@@ -59,28 +60,27 @@ UsersPio.on('connection', function(socket){
 						"key_val" 	: res_info.key_val
 					};
 				} else{
-					console.log('branch else');
+					console.log("[SERVER LOG] Branch else");
 				}
 			});
 
 			UsersPio.emit('mv', mv_obj);
-			
-	
-		//	UsersPio.emit('init_mv', mv_obj);
-			
-		//	UsersPio.emit('init_mv', mv_obj);
+		   //UsersPio.emit('init_mv', mv_obj);	
 		});
+
 		socket.on('init_press_key', function(data){
-			console.log("akldsjflasjdlkfj;asdf/");
+			console.log("[SERVER LOG] Init press key.");
 			console.log(key_val);
+
 			username = data.username;
 			x = data.location_x;
 			y = data.location_y;
 			key_val = data.key_val;
 
+			if(key_val == ENTER)
+         {
+				console.log("[SERVER LOG] Sample press enter.");
 
-			if(key_val == ENTER){
-				console.log("sample press enter");
 				member.find({"accessing" : "true"}).toArray(function(err, results){
 					if(err){
 					
@@ -89,10 +89,9 @@ UsersPio.on('connection', function(socket){
 					}
 				});				
 			}
-		});
-
+		});      
 	});
 });
 
-console.log('broadcastPos.js : http://203.237.179.21:5006');
+console.log("[SERVER LOG] broadcastPos.js : http://203.237.179.21:5006");
 
