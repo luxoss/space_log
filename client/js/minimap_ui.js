4d81c7889f04d8f1bb53b016008e9a618c0a2a1b
luxoss <talitan89@gmail.com>
2016-10-11 04:19:33 +0900

    Modified a file 'minimap_ui.js'

--- client/js/minimap_ui.js
+++ client/js/minimap_ui.js
@@ -10,6 +10,8 @@
 // 각각의 유저 요소와 행성요소를 그려주기위한 함수  
 function drawMinimap(socket)
 {
+   // background width: 3500px, height: 3500px
+   // canvas width: 250px = 3500 / x = ?, height: 250px = 3500 / y = ? 
    var state = $('.minimap_ui').css('display');
    var minimap = document.getElementById('minimap_canvas');
 
@@ -18,14 +20,41 @@ function drawMinimap(socket)
       // 만약 행성이라면, 빨간색 원 모양으로 그리기
 	   // 만약 함선이라면, 노란색 또는 하얀색으로 그리기  
       $('.minimap_ui').show();
-/*
+      /*
+      if(minimap.getContext) 
+      {
+         var ctx = canvas.getContext('2d');
+         var planet = {  // width: 64px, height: 64px
+            x : 0,
+            y : 0
+         };
+         var player = {  // width: 100px, height: 100px;
+            x : 0,
+            y : 0
+         };
+
       socket.planet.emit('ready', {'ready to draw minimap'});
 
       socket.planet.on('planet_res', function(data) {
-         var planetPos = []; 
-         var playerPos = [];
+         ctx.fillStyle = "rgb(255, 0, 0)";
+         ctx.beginPath();
+         ctx.arc(x, y, rad, 0 Math.PI, true) // 시계방향으로 그림
+         ctx.stroke();
       });
-*/
+
+      socket.userPos.on('mv', function(data) {
+         if(user['name'] == data['username'])
+         {
+            ctx.fillStyle = "yellow";
+            ctx.fillRect(user['x'], user['y'], width, height);
+         }
+         else
+         {
+            ctx.fillStyle = "rgb(0, 0, 0)";
+            ctx.fillRect(data['location_x'], data['location_y'], width, height); 
+         }
+      });
+   */       
    }
    else
    {
