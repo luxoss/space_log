3704dd21b36fc43851223c01f457665c2ce2594f
JaesunHan <kapoochino93@gmail.com>
2016-10-16 14:50:38 +0900

    ....Working in devPlanet.js file

--- server/routes/devPlanet.js
+++ server/routes/devPlanet.js
@@ -1,20 +1,13 @@
 var devPlntio = require('socket.io').listen(5003);
-//var MongoClient = require('mongodb').MongoClient;
-
-var getMongoClient = require('./loginForm');
-
-
-
-
+var MongoClient = require('mongodb').MongoClient;
+var planet, mem_info, mem_plan;
 
 devPlntio.on('connection', function(socket){
 	socket.on('add_p', function(data){
 		MongoClient.connect("mongodb://localhost/space_log", function(err, db){
-			var member = db.collection("MEMBER");
-			member.findone({"username" : "jasonhan93"}, function(err, findres){
-				console.log("--=-=--===-=-=-=-===-= findres : ---=-=-=-=-=-=-=-=-=-");
-				console.log(findres);
-			});
+			planet = db.collection("PLANET");
+			mem_info = db.collection("MEM_INFO");
+			mem_plan = db.collection("MEM_PLAN");
 		});
 	});
 	socket.on('rmv_p', function(data){
@@ -22,5 +15,11 @@ devPlntio.on('connection', function(socket){
 	});
 
 });
+
+function dev_p(){
+
+}
+
+//setInterval(dev_p, 600000);
 
 console.log("devPlanet.js : https://203.237.179.21:5003");
