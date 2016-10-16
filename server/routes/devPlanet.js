9f52563840d3059e7ce79e70f1f4fe3b39df9485
JaesunHan <kapoochino93@gmail.com>
2016-10-16 22:25:54 +0900

    ...

--- server/routes/devPlanet.js
+++ server/routes/devPlanet.js
@@ -6,6 +6,7 @@ var p_id, username;
 
 devPlntio.on('connection', function(socket){
 	socket.on('add_p', function(data){
+		console.log('add_p MESSAGE');
 		MongoClient.connect("mongodb://localhost/space_log", function(err, db){
 			planet = db.collection("PLANET");
 			mem_info = db.collection("MEM_INFO");
@@ -18,12 +19,15 @@ devPlntio.on('connection', function(socket){
 				if(err){
 					console.log(err);
 				} else if (result){
+					console.log('result!');
 					if(result.develop == "false"){
+						console.log('result.develop : false');
+						console.log(p_id);
 						planet.update({"p_id" : p_id}, {$set : {"develop" : "true"}});
-						mem_plan.insert({"p_id" : p_id, "username":username});
+						mem_plan.save({"p_id" : p_id, "username":username});
 						devPlntio.emit('add_p_result', {"p_id":p_id, "username":username, "develop":"true"});
 					} else if (result.develop == "true"){
-											
+						console.log("resulte.develop : true");	
 					} else {
 						console.log("PLANET Collection's [develop] field value is not true or false. Please check the value");
 					}	
