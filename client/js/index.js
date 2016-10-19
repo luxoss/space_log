f2892a44a638444d60fe07cd45841afd410c47c6
luxoss <talitan89@gmail.com>
2016-10-20 04:58:34 +0900

    Added a pop-up view code in index.js

--- client/js/index.js
+++ client/js/index.js
@@ -14,9 +14,14 @@ $(document).ready(function() {
       
       $('#main_container').css({
 	      left: ($(window).width() - $('#main_container').outerWidth()) / 2, 
-	      top: ($(window).height() - $('#main_container').outerHeight()) / 2
+	      top : ($(window).height() - $('#main_container').outerHeight()) / 2
       });
       
+      $('#index_pop_up_view').css({
+         left: ($(window).width() - $('#index_pop_up_view').outerWidth()) / 2,
+         top : ($(window).height() - $('#index_pop_up_view').outerHeight()) / 2
+      });
+
    }).resize();		
 
    $("#username").mouseover(function() {
@@ -64,8 +69,9 @@ $(document).ready(function() {
 
       if((user.name == '' ) || (user.password == '')) 
       {
-         alert("아이디와 비밀번호를 입력해주세요.");
-         window.location.reload();
+         popUpMsg("아이디와 비밀번호를 입력해주세요.");
+         //alert();
+         //window.location.reload();
       }
       else 
       {
@@ -77,12 +83,14 @@ $(document).ready(function() {
 	      userInfoSocket.on('join_res', function(data){		
 	         if(data.response == 'true') 
             {
-               alert('회원가입이 완료 되었습니다.');
+               popUpMsg("회원가입이 완료 되었습니다.");
+               //alert();
 	            window.location.reload();
 	         }
             else 
             {
-               alert('해당 아이디가 이미 있습니다.');
+               popUpMsg("해당 아이디가 이미 있습니다."); 
+               //alert();
                window.location.reload();
 	         }
 	      });
@@ -105,13 +113,14 @@ $(document).ready(function() {
          
          if(data.response == "true") 
          {
-            alert(user['name'] + "님 space_log 세계에 오신 것을 환영합니다.");
+            alert(user.name + "님 space_log 세계에 오신 것을 환영합니다.");
             getUserItems(userInfoSocket, user);		
             $(location).attr('href', mainPageUrl);
 	      } 
          else 
          {
-            alert("해당 아이디가 이미 있거나 비밀번호가 틀립니다. 다시 시도해 주세요.");
+            popUpMsg("해당 아이디가 이미 있거나 틀립니다.");
+            //alert();
             window.location.reload();
          }
       });
@@ -151,7 +160,8 @@ $(document).ready(function() {
 	         }
             else 
             {
-	            alert("해당 아이디가 이미 있거나 비밀번호가 틀립니다. 다시 시도해 주세요.");
+               popUpMsg("해당 아이디가 이미 있거나 비밀번호가 틀립니다.");
+	            //alert("해당 아이디가 이미 있거나 비밀번호가 틀립니다. 다시 시도해 주세요.");
 	            window.location.reload();
 	         }
 	      });
@@ -168,7 +178,7 @@ function getUserItems(userInfoSocket, user)
 {
    if(!localStorage) 
    {
-      alert("This browser isn't support localStorage.");
+      popUpMsg("Thisbrower isn't support localStorage.");
    }
    else 
    {	
@@ -202,24 +212,24 @@ function getUserItems(userInfoSocket, user)
    }
 }
 
-//TODO: Check 'join form '
-function pwdCheck() 
+function popUpMsg(msg)
 {
-	var pwd = $.trim($('#password').val());
-	var check_pwd = $.trim($('#check_password').val());
+   var state = $("#index_pop_up_view").css('display');
 
-	if(pwd === check_pwd) 
+   if(state == 'none') 
    {
-		alert("회원가입이 완료되었습니다.");
-      return true;
-	}
-   else 
-   {
-		alert("비밀번호가 일치하지 않습니다. 다시 확인해주세요.");
-      return false;
-	}
+      $("#index_pop_up_view").show();
+      $("#index_pop_up_msg").text(msg);
+      /*
+      $(':text:not([id=username])').val('');
+      $(':text:not([id=username])').val('');
+      */
+   }
+   
+   $("#index_pop_up_hide").click(function() {
+      $("#index_pop_up_view").hide();
+   });
 }
-
 
 
 
