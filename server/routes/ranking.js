55ac92f5fdc42a3b9bf0f6cf79548c078e492d1b
JaesunHan <kapoochino93@gmail.com>
2016-10-29 01:44:31 +0900

    complete ranking system

--- server/routes/ranking.js
+++ server/routes/ranking.js
@@ -1,12 +1,7 @@
-//var rankio = require('socket.io').listen(5008);
+var rankio = require('socket.io').listen(5008);
 var MongoClient = require('mongodb').MongoClient;
-/*
-rankio.on('connection', function(socket){
 
-});
-*/
-
-var expSort = [];
+var expSort = [],  rankArr =[];
 var i=0;
 
 function cal_ranking(){
@@ -16,54 +11,52 @@ function cal_ranking(){
 
 		var m_i_cursor = mem_info.find();
 		var m_p_cursor = mem_plan.find();
-/*
-		m_p_cursor.each(function(err,MPres){
-			if(MPres){
-			//	console.log('ranking.js');
-				member[i++] = MPres;
-			}	
-		});
-
-		for(var z=0; z<member.length;z++){
-		//	console.log(member[z]);
-		} */
-/*
-		m_i_cursor.each(function(err,res){
-			if(res){
-			//	console.log('ranking.js file :::: m_i_cursor.each query is success');
-				member[i++] = res;
-			}
-		});
-
-		for(var z =0; z<member.length; z++){
-			console.log(member[z].username);
-		}
-*/		
+		
 		m_i_cursor.sort({"exp":-1}).toArray(function(err, sortRes){
 			if(err){
 				console.log('sort error!!!!!!!!');
+				console.log(err);
 			} else if(sortRes){
-			//	expSort[i++] = sortRes;			
-			//	console.log(sortRes);
+
 				expSort = sortRes;
+	
 			}
 		});		
 		
 		for(var z=0; z<expSort.length; z++){
 //			console.log(expSort[z].username);	
-			mem_info.update({username:expSort[z].username}, {$set:{rank:z}}, function(err, updateRes){
-			/*	if(updateRes){
-					console.log(updateRes);
-				}*/
+			mem_info.update({username:expSort[z].username}, {$set:{rank:z+1}}, function(err, updateRes){
+	
 			});
 			
 		}
-
-	});
-
-
+	});	
 }
 
 
 //setInterval(cal_ranking, 60000);
 setInterval(cal_ranking, 6000);
+
+
+
+rankio.on('connection', function(socket){
+	socket.on('rank_req', function(data){
+		console.log('reank_req :::::::::::::::');
+	//	console.log(expSort[0].username);
+		MongoClient.connect("mongodb://localhost:27017/space_log", function(err, db){
+			var mem_info = db.collection("MEM_INFO");
+			
+			mem_info.find().sort({"rank":1}).toArray(function(err, rankRes){
+				if(rankRes){
+					rankArr = rankRes;
+				}
+			});			
+			for(var k=0; k<rankArr.length; k++){
+				socket.emit('rank_res', {username: rankArr[k].username, rank:rankArr[k].rank});
+			}
+
+		});		
+	});
+});
+
+
