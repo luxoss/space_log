379988f63ede903389bd0c1e4ff0f78386b8d651
luxoss <talitan89@gmail.com>
2016-10-29 04:41:35 +0900

    Modified a file 'rank_ui.js'

--- client/js/rank_ui.js
+++ client/js/rank_ui.js
@@ -27,22 +27,25 @@ function rankViewLayer(socket)
          var userList = {
             rank : data.rank,
             name : data.username,
+            //score : data.score
          };
 
-         $("#user_list").append("<div id=rank_number_" + userList.rank + "'style='position:inherit; line-height: 100px;'></div>");
-         $("#user_list").append("<div id=rank_username_" + userList.name + "'style='position:inherit; line-height: 100px;'></div>");
+         $("#user_list").append("<div id=rank_number_" + userList.name + "'style='position:aboslute; line-height: 100px;'></div>");
+         $("#user_list").append("<div id=rank_username_" + userList.name + "'style='position:absolute; line-height: 100px;'></div>");
+         //$("#user_score").append("<div id=rank_user_score_" + userList.name + "'stlye='position:absolute; line-height: 100px;'></div>");
 
-         $("#rank_number_" + userList.rank).text(userList['rank']);
+         $("#rank_number_" + userList.name).text(userList['rank']);
          $("#rank_username_" + userList.name).text(userList['name']);
+         //$("#rank_user_score_" + userList.name).text(userList['score']);
 
-         $("#rank_number_" + userList.rank).css({
+         $("#rank_number_" + userList.name).css({
             'background-color' : 'rgba(0, 0, 0, 0.7)',
             'color'            : 'rgba(255, 255, 255, 1)',
             'font-weight'      : 'bold',
             'width'            : 100,
             'height'           : 100,
             'text-align'       : 'center',
-            left               : 10,
+            left               : 0,
             top                : parseInt(0 + styleTop)
          });
 
@@ -56,6 +59,18 @@ function rankViewLayer(socket)
             left               : 110,
             top                : parseInt(0 + styleTop)
          });
+/*
+         $("#rank_user_score_" + userList.name).css({
+            'background-color' : 'rgba(0, 0, 0, 0.7)',
+            'color'            : 'rgba(255, 255, 255, 1)',
+            'font-weight'      : 'bold',
+            'width'            : 100,
+            'height'           : 100,
+            'text-align'       : 'center',
+            left               : 0,
+            top                : parseInt(0 + styleTop)
+         });
+*/
 
          styleTop = parseInt(styleTop + 100);                  
 	   });
